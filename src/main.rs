94265893d780907983128b155c3638319ceb9d3b
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-02-18 23:46:27 -0300

    refactor: extract main cli

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,11 @@
 pub mod adapters;
 pub mod application;
 pub mod domain;
+pub mod infrastructure;
 
-use std::env;
-
-use adapters::{cli_presenter::tasks_cli_presenter, task_adapters::make_http_tasks_gateway};
-use application::{
-    get_tasks_input::GetTasksInput,
-    get_tasks_usecase::{make_get_tasks_service, GetTaskErr, TasksGateway},
-};
-use domain::task::Task;
+use infrastructure::main_cli::run_cli;
 
 #[tokio::main]
 async fn main() {
-    let input = get_input_args();
-    let gateway: TasksGateway = make_http_tasks_gateway();
-    let tasks_result: Result<Vec<Task>, GetTaskErr> = make_get_tasks_service(gateway)(input);
-    let Ok(tasks) = tasks_result else {
-        panic!("Failed to get tasks: {:?}", tasks_result);
-    };
-    tasks_cli_presenter(tasks);
-}
-
-fn get_input_args() -> GetTasksInput {
-    let days_before_today: u64 = env::args()
-        .nth(1)
-        .expect("no sub days specified")
-        .parse::<u64>()
-        .expect("days must be a integer number");
-    GetTasksInput::new(days_before_today)
+    run_cli();
 }
