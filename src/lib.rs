00a16108940eae915c6a3e9c9e85275eddf4e953
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-02-20 18:51:32 -0300

    refactor: extract cli concerns to main

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+use adapters::summary_presenter::TaskMap;
+use application::get_tasks_error::GetTaskErr;
+
 pub mod adapters;
 pub mod application;
 pub mod domain;
 pub mod infrastructure;
 
-pub fn run() {
-    infrastructure::run()
+pub fn run(days_before_today: u64) -> Result<TaskMap, GetTaskErr> {
+    infrastructure::run(days_before_today)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
-use daily_cli::run;
+use std::env;
+
+use daily_cli::{
+    adapters::summary_presenter::{TaskMap, TaskSummary},
+    run,
+};
 
 #[tokio::main]
 async fn main() {
-    run()
+    let days_before_today: u64 = env::args()
+        .nth(1)
+        .expect("no sub days specified")
+        .parse::<u64>()
+        .expect("days must be a integer number");
+    match run(days_before_today) {
+        Ok(map) => print_tasks(&map),
+        Err(err) => eprintln!("Error on get tasks: {}", err),
+    }
+}
+
+fn print_tasks(map: &TaskMap) {
+    print_table_header();
+    for (_, task) in map.iter() {
+        print_task(task);
+    }
+}
+
+fn print_table_header() {
+    println!(
+        "\n{0: <40} | {1: <10} | {2: <14} | {3: <10}",
+        "Description", "Quantity", "Duration (min)", "Tags"
+    );
+}
+
+fn print_task(task: &TaskSummary) {
+    println!(
+        "{0: <40} | {1: <10} | {2: <14} | {3: <10}",
+        task.description, task.quantity, task.total_duration, task.tags[0]
+    );
 }
